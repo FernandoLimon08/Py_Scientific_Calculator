33f007d4bbf8fd074e05de426cc6c027631669d1
Fernando Limon <64861804+LimaLimon90@users.noreply.github.com>
2025-09-05 12:19:54 -0700

    Update Py_Scientific_Calculator_V0.py
    Comments were added to detail the code

--- Py_Scientific_Calculator_V0.py
+++ Py_Scientific_Calculator_V0.py
@@ -2,17 +2,17 @@ import tkinter as tk
 
 root=tk.Tk() #creates the aplication window
 root.title("My calculator")
-message=tk.Label(root,text="Hello World")	#Create a label with the text "Hello World" for the window root
-message.pack() #Shoes the label in the app window
-push_button=tk.Button(root,text="Boton", fg="blue", bg="red")
-push_button.pack(expand=1)
+message=tk.Label(root,text="Hello World")	#Creates a label with the text "Hello World" for the window root
+message.pack() #Shows the label in the app window
+push_button=tk.Button(root,text="Boton", fg="blue", bg="red") #Creates a red button with the label "Boton" in a blue font
+push_button.pack(expand=1) #Shows the button in the app window
 
 root.mainloop() #makes the created window keeps open until its closed
 
-app=tk.Tk()
-target=tk.Label(app,text="Ya aprendi lo basico para crear apps")
-target.pack()
-actions=tk.Button(app,text="Bonon 2")
-actions.pack()
+app=tk.Tk() #creates a second window that apears after the root window is closed
+target=tk.Label(app,text="Ya aprendi lo basico para crear apps") #Creates a label with a simple text
+target.pack() #Shows the label in the app window
+actions=tk.Button(app,text="Boton 2") #Creates a button with the label "boton 2"
+actions.pack() #shows the button in the app window
 
-app.mainloop()
+app.mainloop() 
